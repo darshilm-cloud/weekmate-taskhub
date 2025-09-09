7b248ccb59494f37c247c840ac2c55f17afa59aa
dhruvielsner <dhruvi@elsner.com.au>
2025-09-09 11:40:12 +0530

    project running fixes

--- client/src/containers/Sidebar/SidebarContent.js
+++ client/src/containers/Sidebar/SidebarContent.js
@@ -197,7 +197,7 @@ function SidebarContent({ setSidebarCollapsed, sidebarCollapsed }) {
         children: [
           {
             key: "Analytics-Projects- running",
-            label: "Projects- running",
+            label: "Projects Running",
             onClick: () =>
               handleMenuClick(
                 "Analytics-Projects- running",
