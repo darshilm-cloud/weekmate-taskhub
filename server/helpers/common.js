317182fc7f79f8b3c89bc0a45ad9f100e44aae8b
Kunal <kunal.s@elsner.com>
2026-04-17 17:27:57 +0530

    changes

--- server/helpers/common.js
+++ server/helpers/common.js
@@ -10,6 +10,7 @@ const { Parser } = require("json2csv");
 const json2xls = require("json2xls");
 const { utcDefault } = require("../configs");
 const config = require("../settings/config.json");
+console.log("🚀 ~ config:", config)
 const { errorResponse } = require("./response");
 // const SMTP = mongoose.model("smtp_configs")
 // const { SMTP } = require("../models");
